a0eebe639cd6b1328c277641958703ad14c0405b
Jordane GENGO <jgengo@e1r10p13.42.fr>
2016-11-22 08:23:49 +0100

    test

--- ft_isblank.c
+++
@@ -1,4 +0,0 @@
-int			ft_isblank(char c)
-{
-	return (c == ' ' || c == '\t' || c == '\n');
-}
--- ft_isspace.c
+++
@@ -1,6 +0,0 @@
-#include "libft.h"
-
-int		ft_isspace(char c)
-{
-	return (c == '\r' || c == '\v' || c == '\f' || ft_isblank(c));
-}
